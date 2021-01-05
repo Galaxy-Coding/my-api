4a9aa3c93fed881f524956ef1e7a63e29283f061
Galaxy-Coding <elias.salmi@icloud.com>
2021-01-05 15:32:05 +0000

    added MDN data

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const fetch = require('node-fetch');
 const rateLimit = require('express-rate-limit');
 const settings = { method: 'Get' };
-
+const cheerio = require('cheerio');
 const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000,
 	max: 720
@@ -13,6 +13,8 @@ const limiter = rateLimit({
 
 app.use(limiter);
 app.use(cors());
+
+//xml2js
 app.get('/xml2js', (req, res) => {
 	var URL = decodeURIComponent(req.query.URL);
 	try {
@@ -28,6 +30,51 @@ app.get('/xml2js', (req, res) => {
 						res.json(result);
 					}
 				});
+			});
+	} catch (err) {
+		res.status(400).send({
+			message: 'Bad Request'
+		});
+		console.log(err);
+	}
+});
+
+//api info
+app.get('/info', (req, res) => {
+	try {
+		res.json({
+			uptime: 'https://stats.uptimerobot.com/D6QYvfBy7E',
+			source: 'https://repl.it/@GalaxyCoding/my-api',
+			author: 'galaxy-coding'
+		});
+	} catch (err) {
+		res.status(400).send({
+			message: 'Bad Request'
+		});
+		console.log(err);
+	}
+});
+app.get('/', (req, res) => {
+	try {
+		res.json({ endpoints: ['/xml2js', '/info'] });
+	} catch (err) {
+		res.status(400).send({
+			message: 'Bad Request'
+		});
+		console.log(err);
+	}
+});
+app.get('/mdn', (req, res) => {
+	try {
+		fetch('https://developer.mozilla.org/en-US/docs/Glossary/CSS', settings)
+			.then(res => res.text())
+			.then(text => {
+				const $ = cheerio.load(text);
+				const output = $('p')
+					.first()
+					.text();
+				res.json({ summary: output });
+				const list = [];
 			});
 	} catch (err) {
 		res.status(400).send({
