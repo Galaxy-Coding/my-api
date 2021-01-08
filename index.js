55e8bb12419853abf01559a73a165a1f01bd5504
Galaxy-Coding <elias.salmi@icloud.com>
2021-01-08 01:08:01 +0000

    fixes

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const limiter = rateLimit({
 app.use(limiter);
 app.use(cors());
 
-//xml2js
 app.get('/xml2js', (req, res) => {
 	const URL = decodeURIComponent(req.query.URL);
 	try {
@@ -54,10 +53,17 @@ app.get('/info', (req, res) => {
 		console.log(err);
 	}
 });
+
 app.get('/', (req, res) => {
 	try {
 		res.json({
-			endpoints: ['/xml2js?URL=', '/info', '/mdn?q=', '/mdn-search?q=', '/mdn-search?q=']
+			endpoints: [
+				'/xml2js?URL=',
+				'/info',
+				'/mdn?q=',
+				'/mdn?q=',
+				'/mdn-search?q='
+			]
 		});
 	} catch (err) {
 		res.status(400).send({
@@ -141,9 +147,116 @@ app.get('/js-info-search', (req, res) => {
 				const $ = cheerio.load(text);
 				$('.search-results__title > a ').each(function(i, elem) {
 					list[i] = 'https://javascript.info' + $(this).attr('href');
-					
 				});
-				
+
+				if (!list) {
+					res.status(400).send({
+						message: 'Bad Request'
+					});
+				} else {
+					res.json(list);
+				}
+			});
+	} catch (err) {
+		res.status(400).send({
+			message: 'Bad Request'
+		});
+		console.log(err);
+	}
+});
+
+app.get('/mdn-search', (req, res) => {
+	try {
+		let list = [];
+
+		const q = decodeURIComponent(req.query.q);
+		fetch(`https://developer.mozilla.org/en-US/search?q=${q}`, settings)
+			.then(res => res.text())
+			.then(text => {
+				const $ = cheerio.load(text);
+				$('.result-url a').each(function(i, elem) {
+					list[i] = 'https://developer.mozilla.org' + $(this).attr('href');
+				});
+				if (!list || list.length == 0 || !q || q === undefined) {
+					res.status(400).send({
+						message: 'Bad Request'
+					});
+				} else {
+					res.json(list);
+				}
+			});
+	} catch (err) {
+		res.status(400).send({
+			message: 'Bad Request'
+		});
+		console.log(err);
+	}
+});
+app.get('/genius', (req, res) => {
+	try {
+		let list = [];
+
+		const q = decodeURIComponent(req.query.q);
+		fetch(`https://genius.com/search?q=${q}`, settings)
+			.then(res => res.text())
+			.then(text => {
+				const $ = cheerio.load(text);
+				$('.result-url a').each(function(i, elem) {
+					list[i] = 'https://developer.mozilla.org' + $(this).attr('href');
+				});
+				if (!list || list.length == 0 || !q || q === undefined) {
+					res.status(400).send({
+						message: 'Bad Request'
+					});
+				} else {
+					res.json(list);
+				}
+			});
+	} catch (err) {
+		res.status(400).send({
+			message: 'Bad Request'
+		});
+		console.log(err);
+	}
+}); /*
+app.get('/genius', (req, res) => {
+	try {
+		const q = decodeURIComponent(req.query.q);
+		fetch(`https://genius.com/Trippie-redd-love-scars-lyrics`, settings)
+			.then(res => res.text())
+			.then(text => {
+				const $ = cheerio.load(text);
+				const result = $(".lyrics").text();
+				console.log(result)
+				if (!result ) {
+					res.status(400).send({
+						message: 'Bad Request'
+					});
+				} else {
+					res.json({"lyrics":result});
+				}
+			});
+	} catch (err) {
+		res.status(400).send({
+			message: 'Bad Request'
+		});
+		console.log(err);
+	}
+});
+*/
+app.get('/iogames-space', (req, res) => {
+	try {
+		let list = [];
+
+		const q = decodeURIComponent(req.query.q);
+		fetch(`https://iogames.space/search/node/shooter`, settings)
+			.then(res => res.text())
+			.then(text => {
+				const $ = cheerio.load(text);
+				$('h3 > a').each(function(i, elem) {
+				  console.log($(this).attr('href'))
+					list[i] = {url : $(this).attr('href') , title: $(this).text()}
+				});
 				if (!list ) {
 					res.status(400).send({
 						message: 'Bad Request'
@@ -159,4 +272,5 @@ app.get('/js-info-search', (req, res) => {
 		console.log(err);
 	}
 });
+
 app.listen(4000, () => {});
